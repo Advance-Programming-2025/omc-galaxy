c8926701070c6dfca0ac40d19d6aebc3413ed115
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2025-12-07 19:04:04 +0100

    address energy stack index bug

--- src/components/energy_stacks.rs
+++ src/components/energy_stacks.rs
@@ -8,12 +8,29 @@ pub mod stacks {
     pub(crate) static FREE_CELL_STACK: Mutex<Vec<u32>> = Mutex::new(Vec::new());
     pub(crate) static CHARGED_CELL_STACK: Mutex<Vec<u32>> = Mutex::new(Vec::new());
     pub fn initialize_free_cell_stack(){
+
+        //initialize the free cell stack with all of the possible indexes
         let free_cell_stack = FREE_CELL_STACK.lock();
         match free_cell_stack {
             Ok(mut vec) => {
+                //empty previous values in case of reset
+                vec.clear();
                 for i in 0..N_CELLS {
                     vec.push(i as u32);
                 }
+                //put the indexes in the correct orientation
+                vec.reverse();
+            }
+            Err(err) => {
+                println!("{}", err);
+            }
+        }
+
+        //same thing as above but we just make sure that the vector is empty
+        let charged_cell_stack = CHARGED_CELL_STACK.lock();
+        match charged_cell_stack {
+            Ok(mut vec) => {
+                vec.clear();
             }
             Err(err) => {
                 println!("{}", err);
