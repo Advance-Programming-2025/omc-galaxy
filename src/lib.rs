b40c8a126e10a202a28840a20145988774dfd75e
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2026-01-17 16:04:22 +0100

    module change

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,4 @@ pub mod messages;
 pub use components::Game;
 pub use components::game_loop::run_with_ui;
 pub use utils::{GalaxyTopology, PlanetStatus, ExplorerStatus};
+pub use components::orchestrator::Orchestrator;
